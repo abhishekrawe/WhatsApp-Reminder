9e92add1d2b00becbb1fcb3f0d62e592d832da2e
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-02-11 14:47:49 +0530

    ✨work on Add Reminder and Delete Reminder and give the Unique Id Key to each reminder card

--- reminder-frontend/src/App.js
+++ reminder-frontend/src/App.js
@@ -1,27 +1,45 @@
+import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DateTimePicker from "react-datetime-picker";
 
-import "./App.css";
 
 function App() {
   const [reminderMsg, setReminderMsg] = useState("");
   const [remindAt, setRemindAt] = useState();
+  const [reminderList, setReminderList] = useState([]);
 
-  const addReminder = () => {};
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/getAllReminder")
+      .then((res) => setReminderList(res.data));
+  }, []);
+
+  const addReminder = () => {
+    axios
+      .post("http://localhost:9000/addReminder", { reminderMsg, remindAt })
+      .then(res => setReminderList(res.data));
+      setReminderMsg("")
+      setRemindAt()
+  };
+
+  const deleteReminder = (id) => {
+    axios.post("http://localhost:9000/deleteReminder", { id })
+    .then( res => setReminderList(res.data))
+  };
 
   return (
     <div className="App">
       <div className="homepage">
         <div className="homepage_header">
-          <h1> Remind Me </h1>
+          <h1> Remind Me 🔔  </h1>
           <input
             type="text"
             placeholder="Reminder notes here...."
             value={reminderMsg}
-            onChange={(e) => setReminderMsg}
+            onChange={(e) => setReminderMsg(e.target.value)}
           />
-          {/* e => setNotificationMsg(e.target.value) */}
+         
           <DateTimePicker
             value={remindAt}
             onChange={setRemindAt}
@@ -38,30 +56,17 @@ function App() {
         </div>
 
         <div className="homepage_body">
-          <div className="reminder_card">
-            <h2> Reminder Note</h2>
-            <h3> Remind Me at :</h3>
-            <p> 26/05 / 2021 @2AM</p>
-            <div className="button"> Delete </div>
-          </div>
-          <div className="reminder_card">
-            <h2> Reminder Note</h2>
-            <h3> Remind Me at :</h3>
-            <p> 26/05 / 2021 @2AM</p>
-            <div className="button"> Delete </div>
-          </div>
-          <div className="reminder_card">
-            <h2> Reminder Note</h2>
-            <h3> Remind Me at :</h3>
-            <p> 26/05 / 2021 @2AM</p>
-            <div className="button"> Delete </div>
-          </div>
-          <div className="reminder_card">
-            <h2> Reminder Note</h2>
-            <h3> Remind Me at :</h3>
-            <p> 26/05 / 2021 @2AM</p>
-            <div className="button"> Delete </div>
-          </div>
+        {
+            reminderList.map( reminder => (
+              <div className="reminder_card" key={reminder._id}>
+                <h2>{reminder.reminderMsg}</h2>
+                <h3>Remind Me at:</h3>
+                <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, {timezone:"Asia/Kolkata"})))}</p>
+                <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete</div>
+              </div>
+            ))
+          }
+           
         </div>
       </div>
     </div>
